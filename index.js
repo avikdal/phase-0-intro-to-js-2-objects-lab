76bbf5d8179123ce6a79c91177e1181cbfcc027e
amanda vikdal <vikdal.amanda@gmail.com>
2022-11-09 19:55:22 -0700

    commit tree ccc4f1342647d5ba52f4a9a05da1d4525a100c44

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: {
+        first: "Pheobe",
+        last: "Simonds",
+    },
+    streetAddress: "10440 Butte Dr",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const updateEmployee = {... employee};
+    updateEmployee[key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {... obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
